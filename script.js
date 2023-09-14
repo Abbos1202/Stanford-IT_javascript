280403bf25625f6d45325168134b0bef278a3193
Abbos <abbosibrohimov07@gmail.com>
2023-09-13 21:12:41 -0700

    logical operatotrs / alert() / prompt()

--- script.js
+++ script.js
@@ -1,119 +1,78 @@
-// To'rburchakning maydonini hisoblaydigan dastur tuzing.
+// var a = "1000";
 
-// var boy = 33;
-// var en = 28;
+// console.log(Number(a));
 
-// var S = (boy + en) * 2;
+// logical operators
 
-// console.log(S);
+// || --> or --> yoki
+// && --> and --> va
+// ! --> not --> yo'q
 
-// ==================================================================================>
+// || --> or --> yoki
 
-// Do'koningiz bor. Har bir molning ustiga foiz qo'yib sotasiz. Pechenyeni qanchadir pulga oldingiz. Nechpul foyda ko'rasiz?
+// console.log(true || true); // true
+// console.log(true || false); // true
+// console.log(false || true); // true
+// console.log(false || false); // false
 
-// var pechenyeNarx = 10000000;
-// var ustigaFoiz = 15;
+// var idCard;
+// var faceId;
 
-// var foyda = (pechenyeNarx / 100) * ustigaFoiz;
+// idCard = false;
+// faceId = true;
 
-// console.log(foyda);
+// console.log(idCard || faceId);
 
-// ==================================================================================>
+// && --> and --> va
 
-// Oyligingiz bor. Va o'rtog'ingizdan qarzingiz bor. Va har oy muddatli to'lo'vga olgan uyingizga pul to'lashingiz kerak. Bir yilda qancha pul yig'asiz?
+// console.log(true && true); // true
+// console.log(true && false); // false
+// console.log(false && true); // false
+// console.log(false && false); // false
 
-// var qarz = 1000000;
-// var oylik = 5000000;
-// var muddatliTolov = 2000000;
+// var galstuk;
+// var pul;
 
-// var Pul = (oylik - qarz - muddatliTolov) * 12;
+// galstuk = true;
+// pul = true;
 
-// console.log(Pul);
+// console.log(galstuk && pul);
 
-// ==================================================================================>
+// ! --> not --> yo'q
 
-// Firmangiz har oy qanchadir pul topadi. Reklamaga qanchadir pul sarflaydi. Ishchilarning oyligiga qanchadir pul sarflaydi. Bir yilda qancha pul yig'iladi?
+// var a = 10;
+// var b = 10;
 
-// var kirim = 1100000;
-// var reklama = 1000;
-// var oyliklar = 700000;
+// console.log(a != b);
 
-// var daromad = (kirim - reklama - oyliklar) * 12;
+// console.log(!true);
+// console.log(!false);
 
-// console.log(daromad);
+// % / < / > / <= / >=
 
-// ==================================================================================>
+// var a = 10;
+// var b = 9;
 
-// Har 100 kilometrga qanchadir benzin sarflanadi. Siz qanchadir litr benzin oldingiz. Nechi kilometrga yetadi?
+// console.log(a % b);
+// console.log(a < b);
+// console.log(a > b);
+// console.log(a <= b);
+// console.log(a >= b);
 
-// var olinganBenzin = 56;
-// var har100kmga = 8;
+// alert() / prompt()
 
-// var km = (olinganBenzin / har100kmga) * 100;
+// alert()
 
-// console.log(km);
+// var a = 10;
+// var b = 10;
 
-// ==================================================================================>
+// alert(a + b);
 
+// prompt()
 
-// Benzinning narxi bor. Va har 100 kilometrga qanchadir benzin sarflanadi. Siz qanchadir pulga benzin oldingiz. Nechi kilometrga yetadi?
+// prompt(text, defaultText)
 
-// var narx = 4500;
-// var har100kmga = 9;
-// var pul = 120000;
+// var a = Number(prompt("Birinchi raqamni kiriting"));
+// var b = Number(prompt("Ikkinchi raqamni kiriting"));
 
-// var kilometr = (pul / narx / har100kmga) * 100;
-
-// console.log(kilometr);
-
-// ==================================================================================>
-
-
-// Firmangiz har oy qanchadir pul topadi. Soliqqa topgan pulidan 24% beradi. Reklamaga qanchadir pul sarflaydi. Ishchilarning oyligiga qanchadir pul sarflaydi. Bir yilda qancha pul yig'iladi?
-
-// var kirim = 1100000;
-// var reklama = 1000;
-// var oyliklar = 700000;
-// var soliq = 24;
-
-// var soliqPuli = (kirim / 100) * soliq;
-
-// var daromad = (kirim - soliqPuli - reklama - oyliklar) * 12;
-
-// console.log(soliqPuli);
-// console.log(daromad);
-
-// ==================================================================================>
-
-
-// Mikrofon AliExpressda 40$ turadi. Yetkazib berish narxi - 30%. O'zbekistonda esa AliExpress narxiga 50% qo'yib sotsa bo'ladi. Nechpul foyda ko'rasiz?
-
-// var narx = 40;
-// var yetkazish = 30;
-// var ustigaFoiz = 50;
-
-// var yetkazishNarxi = (narx / 100) * yetkazish;
-
-// var foizliNarx = (narx / 100) * ustigaFoiz;
-
-// var sotuvNarxi = narx - yetkazishNarxi + foizliNarx;
-
-// console.log(yetkazishNarxi);
-// console.log(foizliNarx);
-// console.log(sotuvNarxi);
-
-
-// ==================================================================================>
-
-
-// Do'koningiz bor. Har bir molning ustiga 15% qo'yib sotasiz. Har kun 100000 so'mga ga pechenye sotasiz. Nechi kunda 500000 so'm sof foyda ko'rasiz?
-
-var sotilish = 100000;
-var ustigaFoiz = 15;
-
-var foiz = (sotilish / 100) * 15;
-
-var kun = 500000 / foiz;
-
-console.log(foiz);
-console.log(kun);
+// alert(a + b);

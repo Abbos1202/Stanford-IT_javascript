6b50317667ba31869652b14ef85139d6fb948b9f
Abbos <abbosibrohimov07@gmail.com>
2023-09-20 14:27:50 -0700

    if else

--- script.js
+++ script.js
@@ -1,110 +1,93 @@
-// To'rburchakning maydonini hisoblaydigan dastur tuzing.
+// if / else || else if
 
-// var boy = Number(prompt("Boyi", "33"));
-// var en = Number(prompt("Eni", "28"));
+// var havo = prompt("Ob-havoni kiriting");
 
-// var S = (boy + en) * 2;
+// if (havo === "Yomg'irli") {
+//   alert("Havo yomg'irli, soyabon ovoling");
+// } else if (havo === "qorli") {
+//   alert("Havo qorli, qalinroq kiyinib oling");
+// } else if (havo === "bulutli") {
+//   alert("Havo bulutli, salqin bo'ladi");
+// } else if (havo === "quyoshli") {
+//   alert("Havo quyoshli");
+// } else {
+//   alert("Not found");
+// }
 
-// alert(S);
+// var yosh = Number(prompt("Yoshingizni kiriting"));
 
-// ==================================================================================>
+// if (yosh >= 18) {
+//   alert("Saytga kirishingiz mumkin");
+// } else if (yosh > 18) {
+//   alert("18 yoshdan kichiksiz");
+// } else {
+//   alert("Not found");
+// }
 
-// Do'koningiz bor. Har bir molning ustiga foiz qo'yib sotasiz. Pechenyeni qanchadir pulga oldingiz. Nechpul foyda ko'rasiz?
+// let week = "Seshanba";
 
-// var pechenyeNarx = Number(prompt("Pechenye narxi", "10000000"));
-// var ustigaFoiz = Number(prompt("Ustiga foiz", "15"));
+// if (week === "Dushanba") {
+//   console.log("Haftaning birinchi kuni");
+// } else if (week === "Seshanba") {
+//   console.log("Haftaning ikkinchi kuni");
+// } else if (week === "Chorshanba") {
+//   console.log("Haftaning uchinchi kuni");
+// } else if (week === "Payshanba") {
+//   console.log("Haftaning to'rtinchi kuni");
+// } else if (week === "Juma") {
+//   console.log("Haftaning beshinchi kuni");
+// } else if (week === "Shanba") {
+//   console.log("Haftaning oltinchi kuni");
+// } else if (week === "Yakshanba") {
+//   console.log("Haftaning yettinchi kuni");
+// } else {
+//   console.log("Bunday hafta kuni yo'q");
+// }
 
-// var foyda = (pechenyeNarx / 100) * ustigaFoiz;
+// var a = Number(prompt("Sonni kiriting"));
 
-// alert(foyda);
+// if (a % 2 === 0) {
+//   alert("Juft son");
+// } else if (a % 2 === 1) {
+//   alert("Toq son");
+// } else {
+//   alert("Not found")
+// }
 
-// ==================================================================================>
+// var a = Number(prompt("Sonni kiriting"));
 
-// Oyligingiz bor. Va o'rtog'ingizdan qarzingiz bor. Va har oy muddatli to'lo'vga olgan uyingizga pul to'lashingiz kerak. Bir yilda qancha pul yig'asiz?
+// if (a % 5 === 0 && a % 3 === 0) {
+//   alert("3 ga va 5 ga bo'linadi");
+// } else if (a % 5 === 0) {
+//   alert("5 ga bo'linadi");
+// } else if (a % 3 === 0) {
+//   alert("3 ga bo'linadi");
+// } else {
+//   alert("404 Not found");
+// }
 
-// var qarz = Number(prompt("Qarz", "1000000"));
-// var oylik = Number(prompt("Oylik", "5000000"));
-// var muddatliTolov = Number(prompt("Muddatli to'lov", "2000000"));
+// var taom = prompt("Taomni tanlang / somsa / palov / sarimsoq");
 
-// var Pul = (oylik - qarz - muddatliTolov) * 12;
+// var summa = Number(prompt("Summani kiriting"));
 
-// alert(Pul);
-
-// ==================================================================================>
-
-// Firmangiz har oy qanchadir pul topadi. Reklamaga qanchadir pul sarflaydi. Ishchilarning oyligiga qanchadir pul sarflaydi. Bir yilda qancha pul yig'iladi?
-
-// var kirim = Number(prompt("Kirim", "1100000"));
-// var reklama = Number(prompt("Reklama", "1000"));
-// var oyliklar = Number(prompt("Oyliklar", "700000"));
-
-// var daromad = (kirim - reklama - oyliklar) * 12;
-
-// alert(daromad);
-
-// ==================================================================================>
-
-// Har 100 kilometrga qanchadir benzin sarflanadi. Siz qanchadir litr benzin oldingiz. Nechi kilometrga yetadi?
-
-// var olinganBenzin = Number(prompt("Olingan benzin", "56"));
-// var har100kmga = Number(prompt("Har 100 km ga", "8"));
-
-// var km = (olinganBenzin / har100kmga) * 100;
-
-// alert(km);
-
-// ==================================================================================>
-
-
-// Benzinning narxi bor. Va har 100 kilometrga qanchadir benzin sarflanadi. Siz qanchadir pulga benzin oldingiz. Nechi kilometrga yetadi?
-
-// var narx = Number(prompt("Benzin narxi", "4500"));
-// var har100kmga = Number(prompt("har 100 km ga", "9"));
-// var pul = Number(prompt("Pul", "120000"));
-
-// var kilometr = (pul / narx / har100kmga) * 100;
-
-// alert(kilometr);
-
-// ==================================================================================>
-
-
-// Firmangiz har oy qanchadir pul topadi. Soliqqa topgan pulidan 24% beradi. Reklamaga qanchadir pul sarflaydi. Ishchilarning oyligiga qanchadir pul sarflaydi. Bir yilda qancha pul yig'iladi?
-
-// var kirim = Number(prompt("Kirim", "1100000"));
-// var reklama = Number(prompt("Reklama narxi", "1000"));
-// var oyliklar = Number(prompt("Oyliklar", "700000"));
-// var soliq = Number(prompt("Soliq", "24"));
-
-// var daromad = (kirim - soliqPuli - reklama - oyliklar) * 12;
-
-// alert(daromad);
-
-// ==================================================================================>
-
-
-// Mikrofon AliExpressda 40$ turadi. Yetkazib berish narxi - 30%. O'zbekistonda esa AliExpress narxiga 50% qo'yib sotsa bo'ladi. Nechpul foyda ko'rasiz?
-
-// var narx = Number(prompt("Narx", "40"));
-// var yetkazish = Number(prompt("Yetkazib berish", "30"));
-// var ustigaFoiz = Number(prompt("Ustiga foiz", "50"));
-
-// var sotuvNarxi = narx - yetkazishNarxi + foizliNarx;
-
-
-// alert(sotuvNarxi);
-
-
-// ==================================================================================>
-
-
-// Do'koningiz bor. Har bir molning ustiga 15% qo'yib sotasiz. Har kun 100000 so'mga ga pechenye sotasiz. Nechi kunda 500000 so'm sof foyda ko'rasiz?
-
-var sotilish = Number(prompt("Sotilish", "100000"));
-var ustigaFoiz = Number(prompt("Ustiga foiz", "15"));
-
-var foiz = (sotilish / 100) * 15;
-
-var kun = 500000 / foiz;
-
-alert(kun);
+// if (taom === "palov") {
+//   if (summa >= 15000) {
+//     alert("puliz yetadi");
+//   } else {
+//     alert("puliz yetmaydi");
+//   }
+// } else if (taom === "somsa") {
+//   if (summa >= 3000) {
+//     alert("puliz yetadi");
+//   } else {
+//     alert("puliz yetmaydi");
+//   }
+// } else if (taom === "sarimsoq") {
+//   if (summa >= 12000) {
+//     alert("puliz yetadi");
+//   } else {
+//     alert("puliz yetmaydi");
+//   }
+// } else {
+//   alert("Bunday taom yo'q")
+// }
